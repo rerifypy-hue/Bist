4b86807b48f360c9bc1ab055f8927df6fc6e5e5b
rerifypy-hue <rerifypy@gmail.com>
2026-03-17 21:14:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8377301703:AAEXlRxIzbMIB0A4yvKjvaXNWn2dkQjOMp4"
-API_URL = os.getenv("API_URL")
+API_URL = "https://worker-production-1b5d.up.railway.app"
 
 async def sinyal(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
